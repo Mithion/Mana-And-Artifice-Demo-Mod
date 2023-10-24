8c019b2b20337e68cedd04bf382809db55e24a95
Joh0210 <joh.freund02@gmail.com>
2023-10-24 15:55:25 +0200

    Example faction

---
+++ src/main/java/com/example/examplemod/factions/FactionRIDs.java
@@ -0,0 +1,11 @@
+package com.example.examplemod.factions;
+
+import com.example.examplemod.ExampleMod;
+import net.minecraft.resources.ResourceLocation;
+
+public class FactionRIDs {
+    public static final ResourceLocation FACTION_EXAMPLE_ID = new ResourceLocation(ExampleMod.MODID, "example_faction");
+    public static final ResourceLocation EXAMPLE_MANA = new ResourceLocation(ExampleMod.MODID, "example_mana");
+    public static final ResourceLocation FACTION_EXAMPLE_ICON = new ResourceLocation(ExampleMod.MODID, "textures/guide/faction_icon_example.png");
+    public static final ResourceLocation FACTION_HUD_TEXTURE = new ResourceLocation(ExampleMod.MODID, "textures/gui/gui_manabars.png");
+}
